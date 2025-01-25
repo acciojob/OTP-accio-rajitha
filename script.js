a494597db1d671f82cd3cce534726a1ebb717a0b
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2025-01-25 16:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputs = document.querySelectorAll('.code');
 
   inputs.forEach((input, index) => {
-    input.addEventListener('input', function (e) {
-      if (e.target.value.length === 1 && index < inputs.length - 1) {
+    input.addEventListener('keyup', function (e) {
+      if (e.target.value && index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
-    });
-
-    input.addEventListener('keydown', function (e) {
-      if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+      if (e.key === "Backspace" && index > 0) {
         inputs[index - 1].focus();
       }
     });
   });
-}); /*
+});  /*
 document.addEventListener("DOMContentLoaded", function () {
   const inputs = document.querySelectorAll('.code');
 
